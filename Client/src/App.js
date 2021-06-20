8cf37193d4515003255e0d127a7d8bf6a559f4a7
Naweli Verma <naweliverma7@gmail.com>
2021-06-20 23:29:26 +0530

    msg

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Login from './Login/Login';
+import DoctorSearch from './DoctorSearch/DoctorSearch';
+import {BrowserRouter as Router, Switch, Route,Redirect} from 'react-router-dom'; 
+
 
 function App() {
   return (
     <div className="App">
-    <Login/>
+    
+    <Router> 
+      <Switch>  
+      <Route exact path="/splash" component={Login}/>
+        <Route exact path="/find" component={DoctorSearch}/>
+        <Redirect from="*" to="/splash" />
+      </Switch>
+    </Router>
+    
     </div>
   );
 }
--- Client/src/Login/Login.jsx
+++ Client/src/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, TextField, Button } from '@material-ui/core';
+import  Typography  from '@material-ui/core/Typography';
 import ni from '../Asset/ni.png';
-
+import GoogleLogin from 'react-google-login';
 const useStyles = makeStyles((theme) => ({
 
   image: {
@@ -10,34 +10,58 @@ const useStyles = makeStyles((theme) => ({
     float: 'right',
   },
   textdiv:{
-    
-    paddingLeft: '2rem'
-
+    paddingLeft: '5rem',
   },
   text:{
+   
     paddingTop:'150px',  
-    fontSize: '4.2rem',
+    fontSize: '4rem',
     fontFamily:'Work Sans, sans-serif', 
-    fontWeight: '400'
-  }
+    fontWeight: '400',
+    fontStyle: 'bold'
+  },
+  google: {
+    font:'blue'
+  },
 }));
 
 function Login() {
   const classes = useStyles();
+ 
 
+  const googleSuccess = async (res) => {
+    const result = res?.profileObj;
+    console.log({ result });
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const googleError = () =>
+    alert("Google Sign In was unsuccessful. Try again later");
+  // const onSubmit = async (value, { resetForm }) => {
+  //   try {
+  //     console.log({ value });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <div>
     <div>
-      <img src={ni} className={classes.image} />
+      <img src={ni} className={classes.image} alt ='main'/>
       </div>
+       <div className={classes.textdiv}>
+        <Typography className={classes.text}>We are here for you.</Typography>
       
-
-      <div className={classes.textdiv}>
-        <Typography className={classes.text}>We are here <br/> for you.</Typography>
-        
-        <Button variant="contained">
-        Login with Google
-      </Button>
+      <GoogleLogin
+      clientId ="1031020543503-5ob08b23nh431eocc0vsc3ood4lbk9oh.apps.googleusercontent.com"
+      buttonText="Login with Google"
+      onSuccess={googleSuccess}
+      onFailure={googleError}
+      cookiePolicy={'single_host_origin'} 
+      className ={classes.google}/>
       </div>
 
     </div>

63c1270de6ff24ebe60314da69e40b9ba5ef2a6b
Naweli Verma <naweliverma7@gmail.com>
2021-06-20 19:10:07 +0530

    ms

--- Client/src/Login/Login.jsx
+++ Client/src/Login/Login.jsx
@@ -1,88 +1,45 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { Typography,TextField,Button} from '@material-ui/core';
+import { Typography, TextField, Button } from '@material-ui/core';
+import ni from '../Asset/ni.png';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        height: '100vh',
-      },
-      paper: {
-        margin: theme.spacing(8, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      },
+
+  image: {
+    width: '50%',
+    float: 'right',
+  },
+  textdiv:{
     
-      form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-      },
-      submit: {
-        margin: theme.spacing(3, 0, 2),
-      },
-      image: {
-        backgroundRepeat: 'no-repeat',
-        backgroundColor:
-          theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      },
+    paddingLeft: '2rem'
+
+  },
+  text:{
+    paddingTop:'150px',  
+    fontSize: '4.2rem',
+    fontFamily:'Work Sans, sans-serif', 
+    fontWeight: '400'
+  }
 }));
 
-function Login () {
+function Login() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-    
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-
-           
-          </form>
-        </div>
-      </Grid>
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+    <div>
+    <div>
+      <img src={ni} className={classes.image} />
+      </div>
       
-      </Grid>
+
+      <div className={classes.textdiv}>
+        <Typography className={classes.text}>We are here <br/> for you.</Typography>
+        
+        <Button variant="contained">
+        Login with Google
+      </Button>
+      </div>
+
     </div>
   );
 }
